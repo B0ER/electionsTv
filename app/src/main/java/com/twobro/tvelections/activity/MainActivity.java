def4c89eedeb2ebdfbca9d18dba7e92a90d97878
B0ER <qwertygo85@gmail.com>
2018-07-21 07:12:43 +0300

    Add playing gimn

--- app/src/main/java/com/twobro/tvelections/activity/MainActivity.java
+++ app/src/main/java/com/twobro/tvelections/activity/MainActivity.java
@@ -48,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     presenter = new MainPresenter(this);
+    toSessionFragment();
   }
 
   @Override
--- app/src/main/java/com/twobro/tvelections/fragments/SessionFragment.java
+++ app/src/main/java/com/twobro/tvelections/fragments/SessionFragment.java
@@ -1,27 +1,38 @@
 package com.twobro.tvelections.fragments;
 
 import android.databinding.DataBindingUtil;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.twobro.tvelections.R;
+import com.twobro.tvelections.activity.MainActivity;
 import com.twobro.tvelections.databinding.FragmentSessionBinding;
 
 public class SessionFragment extends Fragment {
+  public static String TAG = "SessionFragment";
   FragmentSessionBinding binding;
+  MediaPlayer mediaPlayer;
 
   public static SessionFragment createFragment(){
     return new SessionFragment();
   }
 
   @Override
-  public void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
+  public void onResume() {
+    super.onResume();
+    mediaPlayer = MediaPlayer.create(getContext(), R.raw.gimn);
+    mediaPlayer.setLooping(false);
+    mediaPlayer.setOnCompletionListener(mp -> {
+      ((MainActivity)getActivity()).toTheWaitingFragment();
+    });
+    mediaPlayer.start();
   }
 
   @Nullable
@@ -31,5 +42,11 @@ public class SessionFragment extends Fragment {
     return binding.getRoot();
   }
 
-
+  @Override
+  public void onDetach() {
+    super.onDetach();
+    mediaPlayer.stop();
+    mediaPlayer.reset();
+    mediaPlayer.release();
+  }
 }
